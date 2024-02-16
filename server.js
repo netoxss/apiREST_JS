1459a67a3c57e57e28a2509d5071469e8cbd685a
netoxss <netop0227@gmail.com>
2024-02-16 13:43:25 -0400

    Versão inicial

--- server.js
+++ server.js
@@ -9,13 +9,34 @@ app.get("/client", function(req, res){
 
 app.get("/client/:id", function(req, res){
     const {id} = req.params;
-    const client = data.find(cli => cli.id == id)
+    const client = data.find(cli => cli.id == id);
 
     if (!client) return res.status(404).json();
     
     else res.json(client);
-    // }
-})
+});
+
+app.post("/client", function(req, res){
+    const {id, name, email} = req.body;
+    res.json({id, name, email})
+
+});
+
+app.put("/client/:id", function(req, res){
+    const {id} = req.params;
+    const client = data.find(cli => client.id = id)
+
+    const {name, email} = req.body;
+
+    if(!client) return res.status(204);
+
+    else 
+    client.name = name, client.email = email; 
+    res.json(client);
+
+    
+
+});
 
 app.listen(3000, function() {
     console.log('Server is running!');
